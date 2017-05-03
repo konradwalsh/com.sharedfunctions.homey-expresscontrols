951a4c93568237705e9e74cf1699974ee3141ed5
konradwalsh <konradwalsh@gmail.com>
2017-05-03 11:27:19 +0100

    Tidy Up

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-﻿// jillesZwave app.js
-
-
-"use strict";
+﻿"use strict";
 const fs = require('fs');
 const path = require('path');
 const util = require('util');
@@ -12,25 +9,10 @@ const EventEmitter = require('events').EventEmitter;
 const Log = require('homey-log').Log;
 const ZwaveDriver = require('homey-zwavedriver');
 
-//const driverMS13E = require('./drivers/MS13E/driver.js');
-//const driverLib = require('./driverGenerator/driverLib.js');
-
-//const update = require('./lib/libClass.js').updateCapabilitiesHomeyDevice;
-//const update = require('./drivers/MS13E/driver.js').updateCapabilitiesHomeyDevice
-
-
-
-//const MS14E = require('./drivers/MS14E.js');
-//const userconfig = require('./userconf.json');
-
-//const signal = jil.signal;
-
 const counter = 0;
 const counter2 = 0;
 
 class App {
-
-
 
     constructor() {
 
@@ -43,43 +25,12 @@ class App {
         this.app = ''
         this.jil = ""
 
-        //signal.on('signal', (frame) =>
-        //  {
-        //      this.lib.log('app on signal  ', frame)
-        //  });
-
-
-
-
         this.init = () => {
             this.lib.log('this is zwave.app', util.inspect(this.homeyDevices, false, null))
-
-           
-
-            
-
-        
-
-
-
          
         } // end init
-
-
-
-
-
     } // end constructor  
-
-
-
-
-
-
-
     // create a generic driver 
-
-
 }  // end class
 
 module.exports = new App();
